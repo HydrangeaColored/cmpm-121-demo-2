5f5a257799f58268734cd872395cdd9e1caec08a
Steven <stevenren999@gmail.com>
2023-10-15 22:30:12 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "This is my game";
+const gameName = "Drawpad";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// canvas creation
+const canvas = document.createElement("canvas");
+const canvasHeight = 256;
+const canvasWidth = 256;
+canvas.height = canvasHeight;
+canvas.width = canvasWidth;
+canvas.id = "canvas";
+const ctx = canvas.getContext("2d")!;
+ctx.fillStyle = "#FFE5B4";
+ctx.fillRect(0, 0, canvas.height, canvas.width);
+app.append(canvas);
