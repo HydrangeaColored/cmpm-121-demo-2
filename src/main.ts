183576a72b1f805f44390599b2b0885890905e9f
Steven <stevenren999@gmail.com>
2023-10-18 03:23:13 -0700

    Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -23,24 +23,29 @@ const canvasPosX = 0;
 const canvasPosY = 0;
 context.fillRect(canvasPosX, canvasPosY, canvas.height, canvas.width);
 app.append(canvas);
+const thinMarkerWidth = 1;
+const thickMarkerWidth = 5;
 
 // geo info objs
 class LineCommand {
   pointsInLine: { x: number; y: number }[];
+  currMarkerWidth: number;
 
-  constructor(currX: number, currY: number) {
+  constructor(currX: number, currY: number, currMarkerWidth: number) {
     this.pointsInLine = [{ x: currX, y: currY }];
+    this.currMarkerWidth = currMarkerWidth;
   }
 
-  display(ctx: CanvasRenderingContext2D) {
+  display(context: CanvasRenderingContext2D) {
+    context.lineWidth = this.currMarkerWidth;
     const firstIndex = 0;
     const { x, y } = this.pointsInLine[firstIndex];
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    context.beginPath();
+    context.moveTo(x, y);
     for (const { x, y } of this.pointsInLine) {
-      ctx.lineTo(x, y);
+      context.lineTo(x, y);
     }
-    ctx.stroke();
+    context.stroke();
   }
 
   drag(x: number, y: number) {
@@ -71,13 +76,14 @@ function redrawCanvas() {
 
 let drawingLine: LineCommand[] = [];
 let undoneLines: LineCommand[] = [];
+let currentMarkerWidth = thinMarkerWidth;
 
 canvas.addEventListener("mousedown", (mouseData) => {
   // mouse is active and start point of new line
   cursor.active = true;
   cursor.x = mouseData.offsetX;
   cursor.y = mouseData.offsetY;
-  const newLine = new LineCommand(cursor.x, cursor.y);
+  const newLine = new LineCommand(cursor.x, cursor.y, currentMarkerWidth);
   drawingLine.push(newLine);
   undoneLines = [];
   canvas.dispatchEvent(redrawLines);
@@ -144,4 +150,23 @@ redoCanvas.addEventListener("click", () => {
     drawingLine.push(undoneLines.pop()!);
     canvas.dispatchEvent(redrawLines);
   }
+});
+
+const newDiv = document.createElement("div");
+app.append(newDiv);
+
+// thin marker
+const thinMarker = document.createElement("button");
+thinMarker.innerHTML = "thin";
+app.append(thinMarker);
+thinMarker.addEventListener("click", () => {
+  currentMarkerWidth = thinMarkerWidth;
+});
+
+// thin marker
+const thickMarker = document.createElement("button");
+thickMarker.innerHTML = "thick";
+app.append(thickMarker);
+thickMarker.addEventListener("click", () => {
+  currentMarkerWidth = thickMarkerWidth;
 });
